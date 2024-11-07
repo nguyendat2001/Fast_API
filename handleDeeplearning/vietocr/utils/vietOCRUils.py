036a6fe067684bac48609d4c023d54d6818fb121
Dat nguyen <ntiendat.it@gmail.com>
2024-11-07 19:37:43 +0700

    [datnt1] fix bug

--- handleDeeplearning/vietocr/utils/vietOCRUils.py
+++ handleDeeplearning/vietocr/utils/vietOCRUils.py
@@ -39,5 +39,5 @@ class VietOCRUtils:
     
     def predictImage(self, image):
         # img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))           
-        return self.predictor.predict(img, return_prob=True) # đối với muốn lấy trả về xác xuất dự đoán
+        return self.predictor.predict(image, return_prob=True) # đối với muốn lấy trả về xác xuất dự đoán
     
